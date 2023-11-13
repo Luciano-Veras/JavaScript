226366fb702f985fd49a02f819e407865877c15b
Luciano Veras <luchoraulveras@gmail.com>
2023-11-13 19:10:49 -0300

    Primer Commit

---
+++ codigo.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const contenedor = document.getElementById("container");
+    let ej = 25;
+    for (let i = 0; i < ej; i++) {
+        contenedor.innerHTML += `<a href="ejercicios/Ejercicio_${i+1}.html"><div class="item">Ejercicio ${i+1}</div></a> `;
+    }   
+});
+
+
